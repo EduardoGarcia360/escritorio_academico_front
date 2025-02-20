2d596ac8583d9e0928f60927452c317d2fe46c37
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-02-20 13:14:38 -0600

    logs

--- src/components/Headers/HeaderStats.js
+++ src/components/Headers/HeaderStats.js
@@ -13,7 +13,7 @@ export default function HeaderStats() {
       objParams: {},
     };
     const response = await api.post("execute-procedure", params);
-    // console.log('stats', response)
+    console.log('stats', response)
     if (response.status === 200) {
       setValores(response.data.results[0]);
     } else {
@@ -42,7 +42,7 @@ export default function HeaderStats() {
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
                   statSubtitle="Estudiantes Inscritos"
-                  statTitle={valores.estudiantes_inscritos ?? ''}
+                  statTitle={valores.estudiantes_inscritos ?? '0'}
                   statIconName="fas fa-school"
                   statIconColor="bg-red-500"
                 />
@@ -50,7 +50,7 @@ export default function HeaderStats() {
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
                   statSubtitle="Maestros Activos"
-                  statTitle={valores.docentes_activos ?? ''}
+                  statTitle={valores.docentes_activos ?? '0'}
                   statIconName="fas fa-users"
                   statIconColor="bg-pink-500"
                 />
@@ -58,7 +58,7 @@ export default function HeaderStats() {
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
                   statSubtitle="% Cuotas Pagadas (mes)"
-                  statTitle={valores.ptje_cuotas_pagadas ?? ''}
+                  statTitle={valores.ptje_cuotas_pagadas ?? '0'}
                   statIconName="fas fa-chart-pie"
                   statIconColor="bg-orange-500"
                 />
@@ -66,7 +66,7 @@ export default function HeaderStats() {
               <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
                 <CardStats
                   statSubtitle="Actividades este Mes"
-                  statTitle={valores.actividades_mes ?? ''}
+                  statTitle={valores.actividades_mes ?? '0'}
                   statIconName="fas fa-bus"
                   statIconColor="bg-lightBlue-500"
                 />
