f646a593625342ecac5b7171fac88574dd791b5e
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-03-03 17:00:04 -0600

    capturar y mostrar ubicaciones

--- src/views/admin/CoordenadasBus/CoordenadasBusPrincipal.js
+++ src/views/admin/CoordenadasBus/CoordenadasBusPrincipal.js
@@ -9,18 +9,35 @@ import busIconUrl from "assets/img/bus.png";
 import noLocationsIconUrl from "assets/img/no_locations.png";
 import { io } from "socket.io-client";
 import { descifrarString } from 'services/codificar.js';
+let contador = 0; // Inicializa un contador
 
 export default function CoordenadasBusPrincipal() {
   const [hasMounted, setHasMounted] = useState(false);
+  const [renderPage, setRenderPage] = useState(false);
   const [coordinates, setCoordinates] = useState([]);
+  const [initialCoordinates, setInitialCoordinates] = useState(null);
+  const [finalCoordinates, setFinalCoordinates] = useState(null);
+  const [intermediateCoordinates, setIntermediateCoordinates] = useState(null);
   const history = useHistory();
   const socketUrl = process.env.REACT_APP_SOCKET_URL;
   const [socket, setSocket] = useState(null);
-  const [registroActividad, setRegistroActividad] = useState({ estado: "F" });
   const [conexionEstablecida, setConexionEstablecida] = useState(false);
   const [codigoSala, setCodigoSala] = useState(null);
   const { idCiclo, idJornadaCiclo, idGrado, idAsignacion, idTransporte } =
     useParams();
+  const limite = 4; // Define el número máximo de ejecuciones
+  const arregloTemporal = [
+    { latitude: 14.623164, longitude: -91.593123 },
+    { latitude: 14.627570, longitude: -91.589371 },
+    { latitude: 14.631976, longitude: -91.585619 },
+    { latitude: 14.636382, longitude: -91.581867 },
+  ]
+  const arregloTemporal2 = [
+    { latitude: 14.640788, longitude: -91.578115 },
+    { latitude: 14.645194, longitude: -91.574363 },
+    { latitude: 14.649600, longitude: -91.570611 },
+    { latitude: 14.654006, longitude: -91.566859 },
+  ]
 
   // Crear íconos personalizados
   const goalIcon = new L.Icon({
@@ -54,6 +71,69 @@ export default function CoordenadasBusPrincipal() {
     iconAnchor: [5, 5],
   });
 
+  const simulacionEnvioUbicacion2 = () => {
+    contador = 0
+
+    const intervalo = setInterval(() => {
+      asignarNuevaCoordenada(arregloTemporal2[contador])
+      contador++; // Incrementa el contador cada segundo
+
+      if (contador === limite) {
+        clearInterval(intervalo);
+      }
+    }, 10000);
+  }
+
+  const simulacionEnvioUbicacion = () => {
+    contador = 0
+
+    const intervalo = setInterval(() => {
+      asignarNuevaCoordenada(arregloTemporal[contador])
+      contador++; // Incrementa el contador cada segundo
+
+      if (contador === limite) {
+        clearInterval(intervalo);
+        simulacionEnvioUbicacion2()
+      }
+    }, 10000);
+  }
+
+  const asignarNuevaCoordenada = (location) => {
+    console.log('ASIGNAR', location)
+    let ubisTmp = coordinates;
+    ubisTmp.push({ lat: location.latitude, lng: location.longitude })
+    setCoordinates(ubisTmp);
+    definirCoordenadas(ubisTmp);
+  };
+
+  const joinRoom = (roomId) => {
+    if (socket && socket.connected) {
+      socket.emit("joinRoom", roomId);
+      // setCurrentRoom(roomId);
+      console.log(`Unido a la sala ${roomId}`);
+      // Limpiamos los mensajes al cambiar de room
+      // setMessages([]);
+    } else {
+      console.error("Socket no está conectado");
+    }
+  };
+
+  const definirCoordenadas = (coordinates) => {
+    const initCoordinates = coordinates.length === 0 ? undefined : coordinates[0];
+    // console.log('initCoordinates', initCoordinates)
+    setInitialCoordinates(initCoordinates)
+
+    const finCoordinates = coordinates.length === 0 ? undefined : coordinates[coordinates.length - 1];
+    // console.log('finCoordinates', finCoordinates)
+    setFinalCoordinates(finCoordinates)
+
+    const interteCoordinates = coordinates.length === 0 ? [] : coordinates.slice(1, -1);
+    // console.log('interteCoordinates', interteCoordinates)
+    setIntermediateCoordinates(interteCoordinates)
+
+    setRenderPage(true)
+  };
+
   useEffect(() => {
     const newSocket = io(socketUrl, {
       transports: ["websocket"],
@@ -63,7 +143,7 @@ export default function CoordenadasBusPrincipal() {
     });
 
     newSocket.on("connect", () => {
-      console.log("Conexión establecida a Socket.io en Seguimiento");
+      console.log("Conexión establecida a Socket.io");
       setConexionEstablecida(true);
     });
 
@@ -77,17 +157,18 @@ export default function CoordenadasBusPrincipal() {
 
     newSocket.on("message", (data) => {
       if (data) {
-        console.log("Mensaje recibido en Seguimiento:", data, typeof data);
-        const decodeData = descifrarString(data);
-        console.log('decodeData', decodeData);
+        console.log(`Mensaje recibido de WebSocket: ${typeof data}`);
+        try {
+          const decodeData = descifrarString(data);
+          // console.log('decodeData', decodeData);
 
-        if (decodeData) {
-          const dataParse = JSON.parse(decodeData);
-          console.log('dataParse', dataParse);
-
-          // const logPagina = `${dataParse.texto} ${dataParse.location}`;
-
-          // handleSave(dataParse.location);
+          if (decodeData) {
+            const dataParse = JSON.parse(decodeData);
+            console.log('dataParse', dataParse);
+            asignarNuevaCoordenada(JSON.parse(dataParse.location));
+          }
+        } catch (error) {
+          console.error('error parse', error.message)
         }
       }
     });
@@ -104,28 +185,6 @@ export default function CoordenadasBusPrincipal() {
       joinRoom(codigoSala);
     }
   }, [hasMounted, codigoSala, conexionEstablecida]);
-
-  const fetchCoordinates = async () => {
-    try {
-      const response = await api.get(
-        `coordenadasbus/transporte/${idTransporte}`
-      );
-      console.log("ubis", response);
-      if (response.data.length === 0) {
-        setCoordinates([]);
-      } else {
-        const tmp = response.data.map((location) => ({
-          ...location,
-          lat: location.latitud,
-          lng: location.longitud,
-        }));
-        // console.log("tmp", tmp);
-        setCoordinates(tmp);
-      }
-    } catch (error) {
-      console.error("Error fetching coordinates:", error);
-    }
-  };
 
   useEffect(() => {
     if (
@@ -145,101 +204,107 @@ export default function CoordenadasBusPrincipal() {
       );
       console.log("registro", response);
       const actividad = response.data;
-      setRegistroActividad(actividad);
+      // setRegistroActividad(actividad);
       setCodigoSala(
         `ACTIVIDAD-${actividad.id_asignacion_transporte}-${actividad.id_asignacion}-${actividad.id_bus}-${actividad.id_docente}`
       );
       setHasMounted(true);
     };
 
+    const fetchCoordinates = async () => {
+      try {
+        const response = await api.get(
+          `coordenadasbus/transporte/${idTransporte}`
+        );
+        console.log("ubis", response);
+        if (response.data.length === 0) {
+          setCoordinates([]);
+          definirCoordenadas([]);
+        } else {
+          const tmp = response.data.map((location) => ({
+            ...location,
+            lat: location.latitud,
+            lng: location.longitud,
+          }));
+          // console.log("tmp", tmp);
+          setCoordinates(tmp);
+          definirCoordenadas(tmp);
+        }
+      } catch (error) {
+        console.error("Error fetching coordinates:", error);
+      }
+    };
+
     consultarRegistro();
     fetchCoordinates();
   }, [idCiclo, idJornadaCiclo, idGrado, idAsignacion, idTransporte, history]);
 
-  if (!hasMounted) {
-    return null; // Esperar hasta que los datos estén disponibles
-  }
-
-  const joinRoom = (roomId) => {
-    if (socket && socket.connected) {
-      socket.emit("joinRoom", roomId);
-      // setCurrentRoom(roomId);
-      console.log(`Unido a la sala ${roomId}`);
-      // Limpiamos los mensajes al cambiar de room
-      // setMessages([]);
-    } else {
-      console.error("Socket no está conectado");
-    }
-  };
-
-  const initialCoordinates =
-    coordinates.length === 0 ? undefined : coordinates[0];
-  //   console.log('initialCoordinates', initialCoordinates)
-  const finalCoordinates =
-    coordinates.length === 0 ? undefined : coordinates[coordinates.length - 1];
-  //   console.log('finalCoordinates', finalCoordinates)
-  const intermediateCoordinates =
-    coordinates.length === 0 ? [] : coordinates.slice(1, -1);
-  //   console.log('intermediateCoordinates', intermediateCoordinates)
+  // useEffect(() => {
+  //   if (renderPage) {
+  //     simulacionEnvioUbicacion();
+  //   }
+  // }, [renderPage]);
 
   return (
     <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
-        <MapContainer
-          id="contenedorMapaLeaflet"
-          center={
-            !finalCoordinates
-              ? { lat: 14.641871637588684, lng: -90.5130113026698 }
-              : finalCoordinates
-          }
-          zoom={13}
-          style={{ height: "900px", width: "100%" }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="© OpenStreetMap contributors"
-          />
-
-          {/* Marcador inicial */}
-          {initialCoordinates && (
-            <Marker
-              position={[initialCoordinates.lat, initialCoordinates.lng]}
-              icon={goalIcon}
-            >
-              <Popup>Hola soy el inicio</Popup>
-            </Marker>
-          )}
-
-          {/* Marcador final */}
-          {finalCoordinates && (
-            <Marker
-              position={[finalCoordinates.lat, finalCoordinates.lng]}
-              icon={busIcon}
-            >
-              <Popup>Hola soy final</Popup>
-            </Marker>
-          )}
-
-          {/* Marcador sin ubicaciones */}
-          {!initialCoordinates && !finalCoordinates && (
-            <Marker
-              position={[14.641871637588684, -90.5130113026698]}
-              icon={noLocationsIcon}
-            >
-              <Popup>No hay ubicaciones disponibles</Popup>
-            </Marker>
-          )}
-
-          {/* Marcadores intermedios */}
-          {intermediateCoordinates.length > 0 &&
-            intermediateCoordinates.map((coord, index) => (
+        {
+          renderPage === true && (<MapContainer
+            id="contenedorMapaLeaflet"
+            center={
+              !finalCoordinates
+                ? { lat: 14.641871637588684, lng: -90.5130113026698 }
+                : finalCoordinates
+            }
+            zoom={13}
+            style={{ height: "900px", width: "100%" }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="© OpenStreetMap contributors"
+            />
+  
+            {/* Marcador inicial */}
+            {initialCoordinates && (
               <Marker
-                key={index}
-                position={[coord.lat, coord.lng]}
-                icon={pointIcon}
-              />
-            ))}
-        </MapContainer>
+                position={[initialCoordinates.lat, initialCoordinates.lng]}
+                icon={goalIcon}
+              >
+                <Popup>Hola soy el inicio</Popup>
+              </Marker>
+            )}
+  
+            {/* Marcador final */}
+            {finalCoordinates && (
+              <Marker
+                position={[finalCoordinates.lat, finalCoordinates.lng]}
+                icon={busIcon}
+              >
+                <Popup>Hola soy final</Popup>
+              </Marker>
+            )}
+  
+            {/* Marcador sin ubicaciones */}
+            {!initialCoordinates && !finalCoordinates && (
+              <Marker
+                position={[14.641871637588684, -90.5130113026698]}
+                icon={noLocationsIcon}
+              >
+                <Popup>No hay ubicaciones disponibles</Popup>
+              </Marker>
+            )}
+  
+            {/* Marcadores intermedios */}
+            {intermediateCoordinates.length > 0 &&
+              intermediateCoordinates.map((coord, index) => (
+                <Marker
+                  key={index}
+                  position={[coord.lat, coord.lng]}
+                  icon={pointIcon}
+                />
+              ))}
+          </MapContainer>)
+        }
       </div>
       <div className="mt-4 flex justify-center">
         <button
